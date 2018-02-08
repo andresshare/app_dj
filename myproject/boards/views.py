40d9ee8050ad0d2f191db741c85d9c3f2e649380
andresshare <andresshare@gmail.com>
2018-02-08 18:47:53 -0500

    Estructura visual

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+from .models import Board
+
 
 def home(request):
-    return HttpResponse("Message")
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
